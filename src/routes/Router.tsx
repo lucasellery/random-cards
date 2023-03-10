ca733722df30c340aced7bb8f718145bbba41aba
lucasellery-osf <lucas.ellery@osf.digital>
2023-03-10 18:12:37 -0300

    (setup) project created and configured

---
+++ src/routes/Router.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import { Board } from "../pages/Board";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/board",
+    element: <Board />,
+  },
+]);
